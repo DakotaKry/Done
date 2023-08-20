6a361049829f1531b67c61fb0c8bb1f245abfbf3
Dakota Kryzanowski <dakotarj@outlook.com>
2023-08-19 21:56:45 -0600

    Added date dialouge selector

--- app/src/main/java/com/example/done/ui/quit/AddQuitFragment.kt
+++ app/src/main/java/com/example/done/ui/quit/AddQuitFragment.kt
@@ -1,14 +1,25 @@
 package com.example.done.ui.quit
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import android.widget.TimePicker
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.done.R
 import com.example.done.databinding.FragmentAddQuitBinding
 import com.example.done.databinding.FragmentQuitBinding
+import com.google.android.material.textfield.TextInputEditText
+import java.util.Calendar
+import java.util.Date
 
 
 /**
@@ -16,7 +27,7 @@ import com.example.done.databinding.FragmentQuitBinding
  * Use the [AddQuitFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AddQuitFragment : DialogFragment() {
+class AddQuitFragment : DialogFragment(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private var _binding: FragmentAddQuitBinding? = null
 
@@ -31,7 +42,59 @@ class AddQuitFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_quit, container, false)
+
+        _binding = FragmentAddQuitBinding.inflate(inflater, container, false)
+        val root = binding.root
+
+
+        var quiteTextTitle: TextInputEditText = binding.newQuitTitleEditText
+        val cancelBtn: Button = binding.newQuitCancelButton
+        val doneBtn: Button = binding.newQuitDoneButton
+        val dateTimeBtn: Button = binding.newQuitStartDateButton
+        var startDateText: TextView = binding.newQuitStartDateText
+
+        var calendar: Calendar = Calendar.getInstance()
+        var year = calendar.get(Calendar.YEAR)
+        var month = calendar.get(Calendar.MONTH)
+        var day = calendar.get(Calendar.DAY_OF_MONTH)
+
+
+
+
+        dateTimeBtn.setOnClickListener{
+
+            Log.d("AddQuitFrag", "dateTimeBtn clicked!")
+
+
+            var datePickerDialog: DatePickerDialog = DatePickerDialog(requireActivity(), DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+                Toast.makeText(requireActivity(), """$dayOfMonth - ${monthOfYear + 1} - $year""", Toast.LENGTH_LONG).show()
+
+                startDateText.text = "$dayOfMonth/${monthOfYear + 1}/$year"
+
+            }, year, month, day)
+
+            datePickerDialog.show()
+
+
+        }
+
+        Log.d("AddQuitFrag", "datePickerDialog Date: $year$month$day")
+
+
+
+
+        return root
+
+
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        TODO("Not yet implemented")
     }
 
 
