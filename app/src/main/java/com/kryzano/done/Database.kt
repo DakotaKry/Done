eeb595001667447bcea3f026372745ac84a20cda
Dakota Kryzanowski <dakotarj@outlook.com>
2023-08-19 21:56:49 -0600

    Implemented User and Database methods with database calls

--- app/src/main/java/com/kryzano/done/Database.kt
+++ app/src/main/java/com/kryzano/done/Database.kt
@@ -7,7 +7,8 @@ import java.util.Calendar
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
-class Database {
+// TODO: Could make a private attribute uid, instead of always passing it to the method
+class Database() {
 
     // Gets our Firebase Instance
     private val fdb = Firebase.firestore
@@ -20,42 +21,47 @@ class Database {
      * Args: user: User
      * returns: None
      */
-    fun createNewUser(user: User){
+    fun pushUsername(uid: String, username: String){
 
-        // get what we need from the User object
-        val identifiers = user.getIdentifiers()
-        val quitList = user.getQuitList()
+        // Sets Username //
 
         // creates a hashmap for the users field
         val usernameData = hashMapOf(
-            "username" to identifiers.second
+            "username" to username
         )
 
-
-        Log.d("Database", identifiers.first)
-        Log.d("Database", identifiers.second)
-
-        // sets the usernameData
-        fdbUsers.document("/${identifiers.first}").set(usernameData)
-
-        // uses indexing for unique identifiers
-        for (i in 0 until quitList.size) {
-
-            // creates a hashmap for every quit field
-            val quitData = hashMapOf<String, Any>(
-                "title" to quitList[i].getTitle(), // title stored as a string
-                "time" to quitList[i].getCalendar().timeInMillis // calendar stored as a long
-            )
-
-            // sets
-            fdbUsers.document("/${identifiers.first}")
-                .collection("/quits").document("/${i}").set(quitData)
-
-        }
-
+        fdbUsers.document("/${uid}").set(usernameData)
 
     }
 
+    /**
+     * Fetchs the username from a given uid (might be redundant since
+     *  one can also do FirebaseUser.displayName
+     *
+     *  Args: uid:String
+     *  Return: None
+     *  Result: String
+     */
+    fun fetchUsername(uid: String, callback:(String) -> Unit){
+
+        val docRef = fdb.collection("users").document("/$uid")
+        docRef.get().addOnSuccessListener {
+
+            val result = it
+
+            // try to get the username, if null then returns an empty string
+            try{
+                callback(result.data!!["username"] as String)
+
+            } catch(e: NullPointerException){
+
+                callback("")
+
+            }
+
+        }
+
+    }
     fun getUsername(email: String): String{
 
         var username = ""
@@ -79,30 +85,23 @@ class Database {
 
     }
 
+
+
     /**
-     * gets the quits given a users email address.
-     * method waits for results and then returns
+     * Fetches the quits given a users uid (usually email).
+     * method gives a call back for asyn
+     * see: https://stackoverflow.com/questions/57330766/why-does-my-function-that-calls-an-api-or-launches-a-coroutine-return-an-empty-o/57330767#57330767
      *
-     * Args: email: String
-     * Return: quitList: ArrayList<Quit>
+     * Args: uid: String
+     * Return: None
+     * Result: ArrayList<Quit>
      */
-    fun getQuits(email: String): ArrayList<Quit>{
+    fun fetchQuits(uid: String, callback:(ArrayList<Quit>) -> Unit){
 
-        var quitList: ArrayList<Quit> = ArrayList()
-
-        try {
-
-            val docRef = fdb.collection("users").document("/$email")
-            val task = docRef.collection("/quits").get()
-            while (!task.isComplete){
-                Thread.sleep(100)
-                // Does nothing as we wait for the task to complete
-                // TODO: Consider Async design using Livedata so not to freeze thread
-                // https://stackoverflow.com/questions/57330766/why-does-my-function-that-calls-an-api-or-launches-a-coroutine-return-an-empty-o/57330767#57330767
-            }
-            val result = task.result
-
-            // Goes through all quits and populates empty quitList for return
+        val docRef = fdb.collection("users").document("/$uid")
+        docRef.collection("/quits").get().addOnSuccessListener {
+            var quitList: ArrayList<Quit> = ArrayList()
+            val result = it
             for (document in result) {
                 Log.d("DatabaseFetch","ID: ${document.id}, Data: ${document.data}")
                 val title = document.data["title"].toString() // get the title
@@ -115,13 +114,135 @@ class Database {
 
             }
 
-
-        } catch (exception: Exception) {
-            Log.e("Database", exception.toString())
+            callback(quitList)
         }
 
-        return quitList
+    }
 
+    /**
+     * Fetches the friends given a users uid (usually email).
+     * method gives a call back for asyn
+     * see: https://stackoverflow.com/questions/57330766/why-does-my-function-that-calls-an-api-or-launches-a-coroutine-return-an-empty-o/57330767#57330767
+     *
+     * Args: uid: String
+     * Return: None
+     * Result: ArrayList<String>
+     */
+    fun fetchFriends(uid: String, callback:(ArrayList<String>) -> Unit){
+
+        val docRef = fdb.collection("users").document("/$uid")
+        docRef.collection("/friends").get().addOnSuccessListener {
+            var friendList: ArrayList<String> = ArrayList()
+            val result = it
+            for (document in result) {
+                Log.d("DatabaseFetch","ID: ${document.id}, Data: ${document.data}")
+                val fuid = document.data["uid"].toString() // get the title
+
+                Log.d("DatabaseFetch","friend: ${fuid}")
+                friendList.add(fuid) // add the Quit to list
+
+            }
+
+            callback(friendList)
+        }
+
+    }
+
+    /**
+     * Fetches the blocks given a users uid (usually email).
+     * method gives a call back for asyn
+     * see: https://stackoverflow.com/questions/57330766/why-does-my-function-that-calls-an-api-or-launches-a-coroutine-return-an-empty-o/57330767#57330767
+     *
+     * Args: uid: String
+     * Return: None
+     * Result: ArrayList<String>
+     */
+    fun fetchBlocks(uid: String, callback:(ArrayList<String>) -> Unit){
+
+        val docRef = fdb.collection("users").document("/$uid")
+        docRef.collection("/blocks").get().addOnSuccessListener {
+            var blockList: ArrayList<String> = ArrayList()
+            val result = it
+            for (document in result) {
+                Log.d("DatabaseFetch","ID: ${document.id}, Data: ${document.data}")
+                val buid = document.data["uid"].toString() // get the title
+
+                Log.d("DatabaseFetch","block: ${buid}")
+                blockList.add(buid) // add the Quit to list
+
+            }
+
+            callback(blockList)
+        }
+
+    }
+
+    fun addQuit(uid: String, quit: Quit){
+
+        val quitData = hashMapOf(
+            "title" to quit.getTitle(),
+            "time" to quit.getCalendar().timeInMillis
+        )
+
+
+        fdbUsers.document("/${uid}").collection("/quits")
+            .document(quit.getTitle()).set(quitData)
+        Log.d("DatabasePush","addQuit: $quitData")
+
+    }
+
+    fun removeQuit(uid: String, quit: Quit){
+
+        fdbUsers.document("/$uid").collection("/quits")
+            .document(quit.getTitle()).delete()
+
+
+    }
+
+    fun addFriend(uid: String, friend: String){
+
+        // TODO: Add checking that they exist
+
+        val friendData = hashMapOf(
+            "uid" to friend
+        )
+
+        fdbUsers.document("/${uid}").collection("/friends")
+            .document(friend).set(friendData)
+
+    }
+
+    fun removeFriend(uid: String, friend: String){
+
+        fdbUsers.document("/$uid").collection("/friends")
+            .document(friend).delete()
+
+
+    }
+
+    fun addBlock(uid: String, block: String){
+
+        // TODO: Add checking that they exist
+
+        val blockData = hashMapOf(
+            "uid" to block
+        )
+
+        fdbUsers.document("/${uid}").collection("/blocks")
+            .document(block).set(blockData)
+
+    }
+
+    fun removeBlock(uid: String, block: String){
+
+        fdbUsers.document("/$uid").collection("/blocks")
+            .document(block).delete()
+
+
+    }
+
+    fun removeUser(uid: String){
+        fdbUsers.document("/$uid").delete()
     }
 
 }
--- app/src/main/java/com/kryzano/done/User.kt
+++ app/src/main/java/com/kryzano/done/User.kt
@@ -1,27 +1,125 @@
 package com.kryzano.done
 
-class User {
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.UserProfileChangeRequest.Builder
+import java.lang.Exception
 
-    private var email: String = ""  // email
-    private var username: String = "" // username as a display name (will be unique)
+class User(fuser: FirebaseUser) {
+
+    private var fuser = fuser // Only ever used for changing displayname/username
+    private var uid = fuser.uid
+    private val db = Database()
+
+    private var username = "Dummy"
     private var friends: ArrayList<String> = ArrayList() // string list of friends emails
     private var quitList: ArrayList<Quit> = ArrayList() // quit list of users quit
     private var blockList: ArrayList<String> = ArrayList() // list of people blocked, handled all locally
 
-    constructor(email: String, username: String) {
-        this.email = email
-        this.username = username
+    fun refreshFuser(fuser: FirebaseUser){
+        this.uid = fuser.uid
+        this.fuser = fuser
     }
+
+    fun getUid(): String {
+        return this.uid
+    }
+
+    /**
+     * Gets and overrides User with everything from Firestore
+     *
+     * Args: None
+     * Returns: None
+     */
+    fun initialize() {
+
+        for ( friend in friends ){
+            db.addFriend(this.uid, friend)
+        }
+
+        for ( quit in quitList ){
+            db.addQuit(this.uid, quit)
+        }
+
+        for ( block in blockList ){
+            db.addBlock(this.uid, block)
+        }
+
+
+        db.fetchUsername(this.uid){ result ->
+            this.username = result
+        }
+
+        db.fetchFriends(this.uid){ result ->
+            this.friends = result
+        }
+        db.fetchQuits(this.uid){ result ->
+            this.quitList = result
+        }
+        db.fetchBlocks(this.uid){ result ->
+            this.blockList = result
+        }
+
+    }
+
+    fun getUsername(): String {
+        return this.username
+    }
+
+    fun setUsername(newUsername: String){
+
+        val request = UserProfileChangeRequest.Builder().setDisplayName(newUsername).build()
+        try {
+            fuser.updateProfile(request)
+            this.username = newUsername
+            db.pushUsername(this.uid, newUsername)
+
+        } catch (e: Exception){
+
+        }
+
+    }
+
 
     fun addQuit(quit: Quit){
 
         quitList.add(quit)
+        db.addQuit(this.uid, quit)
 
     }
 
     fun removeQuit(quit: Quit){
 
         quitList.remove(quit)
+        db.removeQuit(this.uid, quit)
+
+    }
+
+    fun addFriend(friend: String){
+
+        friends.add(friend)
+        db.addFriend(this.uid, friend)
+
+    }
+
+    fun removeFriend(friend: String){
+
+        friends.remove(friend)
+        db.removeFriend(this.uid, friend)
+
+    }
+
+    fun addBlock(block: String){
+
+        blockList.add(block)
+        db.addBlock(this.uid, block)
+
+    }
+
+    fun removeBlock(block: String){
+
+        blockList.remove(block)
+        db.removeBlock(this.uid, block)
 
     }
 
@@ -35,10 +133,6 @@ class User {
 
     fun getBlockList(): ArrayList<String> {
         return blockList
-    }
-
-    fun getIdentifiers(): Pair<String, String> {
-        return Pair(email, username)
     }
 
 }
