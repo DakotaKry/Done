5b4e32bff9013c79bbd211e038375a1f8343fa04
Dakota Kryzanowski <dakotarj@outlook.com>
2023-08-19 21:56:52 -0600

    Added quitEdit attribute and methods for editing quits

--- app/src/main/java/com/kryzano/done/MainViewModel.kt
+++ app/src/main/java/com/kryzano/done/MainViewModel.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModel
 class MainViewModel : ViewModel() {
 
     private lateinit var user: User
+    private var quitEdit: Quit? = null
 
     fun setUser(user: User){
         this.user = user
@@ -16,6 +17,18 @@ class MainViewModel : ViewModel() {
 
     fun getUser(): User{
         return this.user
+    }
+
+    fun setQuitEdit(quit: Quit){
+        this.quitEdit = quit
+    }
+
+    fun getQuitEdit(): Quit?{
+        return this.quitEdit
+    }
+
+    fun removeQuitEdit(){
+        this.quitEdit = null
     }
 
 
