a057de4115a2f4234d6cac463803aad6ebeab5fa
Dakota Kryzanowski <dakotarj@outlook.com>
2023-08-19 21:56:44 -0600

    Added comments and added a fragment pop up for adding a new quit

--- app/src/main/java/com/example/done/MainActivity.kt
+++ app/src/main/java/com/example/done/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.done
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -16,10 +20,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Boolean to see if we need to freeze Nav Bar init to false
+        var freezeNav: MutableLiveData<Boolean> = MutableLiveData(false)
+
+
         val navView: BottomNavigationView = binding.navView
+
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
@@ -31,5 +41,15 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // Observes when the live data changes
+        freezeNav.observe(this, Observer<Boolean>(){
+            Log.d("LiveData","freezeNav observed!")
+        })
+
+        // Change the value of the live data To be implemented with ViewModel to change across fragments
+        freezeNav.postValue(true)
+
+
     }
 }
--- app/src/main/java/com/example/done/ui/quit/QuitFragment.kt
+++ app/src/main/java/com/example/done/ui/quit/QuitFragment.kt
@@ -6,15 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.forEach
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.done.MainActivity
+import com.example.done.R
 import com.example.done.databinding.FragmentQuitBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class QuitFragment : Fragment() {
 
     private var _binding: FragmentQuitBinding? = null
-
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -30,14 +36,23 @@ class QuitFragment : Fragment() {
         _binding = FragmentQuitBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        quitViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
 
+
+
+        // On click listener for the add new quit button
         val addQuitButton: FloatingActionButton = binding.newButtonQuit
         addQuitButton.setOnClickListener {
             Log.d("QuitFrag", "addQuitButton Clicked!")
+
+
+            val addQuitFragment: Fragment = AddQuitFragment()
+            val addQuitTransaction: FragmentTransaction = childFragmentManager.beginTransaction().apply {
+                add(R.id.fragment_fragment_quit, addQuitFragment)
+                commit()
+            }
+
+
+
         }
 
         return root
